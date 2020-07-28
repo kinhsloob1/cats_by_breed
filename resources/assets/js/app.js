eaa413a44f373c22555231f722d040997f6e5add
okafor obinna kingsley <kingsloob1@gmail.com>
2020-07-28 23:32:13 +0100

    add vue loading stylesheet

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,14 @@
 import VueApollo from 'vue-apollo'
 import apolloProvider from '@/config/graphql_apollo'
 import App from '@/components/entry.vue';
-import Loader from 'vue-loading-overlay';
+import Loading from 'vue-loading-overlay';
 import Vue from 'vue';
+
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 //Load Plugins
 Vue.use(VueApollo)
-Vue.use(Loader);
+Vue.use(Loading);
 
 export const vm = new Vue({
     el: '#app',
