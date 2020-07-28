4dd6bb316567ec39cfc5c1d650e63093b2db783e
okafor obinna kingsley <kingsloob1@gmail.com>
2020-07-28 23:37:57 +0100

    install and configure vue toasted

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,12 +3,26 @@ import apolloProvider from '@/config/graphql_apollo'
 import App from '@/components/entry.vue';
 import Loading from 'vue-loading-overlay';
 import Vue from 'vue';
+import Toasted from 'vue-toasted';
 
 import 'vue-loading-overlay/dist/vue-loading.css';
 
 //Load Plugins
 Vue.use(VueApollo)
 Vue.use(Loading);
+
+Vue.use(Toasted, {
+  position: 'bottom-right',
+  action : {
+    text : 'Cancel',
+    onClick : (e, toastObject) => {
+      toastObject.goAway(0);
+    }
+  },
+  keepOnHover: true,
+  duration: 5000,
+  singleton: false,
+})
 
 export const vm = new Vue({
     el: '#app',
