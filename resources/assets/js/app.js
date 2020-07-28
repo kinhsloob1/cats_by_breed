126a68372aeb53ad577f67a7f675dc206e3185b7
okafor obinna kingsley <kingsloob1@gmail.com>
2020-07-28 23:31:02 +0100

    configure vue loading plugin

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,12 @@
 import VueApollo from 'vue-apollo'
 import apolloProvider from '@/config/graphql_apollo'
 import App from '@/components/entry.vue';
+import Loader from 'vue-loading-overlay';
 import Vue from 'vue';
 
 //Load Plugins
 Vue.use(VueApollo)
+Vue.use(Loader);
 
 export const vm = new Vue({
     el: '#app',
