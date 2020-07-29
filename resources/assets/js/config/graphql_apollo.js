528d8692ad330a827ef617a07c715f3cd29ea2a4
okafor obinna <kingsloob1@gmail.com>
2020-07-29 16:07:05 +0100

    eslint fix

--- resources/assets/js/config/graphql_apollo.js
+++ resources/assets/js/config/graphql_apollo.js
@@ -1,27 +1,27 @@
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import VueApollo from "vue-apollo";
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
 
 /* let token = document.head.querySelector('meta[name="csrf-token"]') */
 
 const httpLink = new HttpLink({
-    headers: {
-        "X-CSRF-TOKEN": "",
-        "X-Requested-With": "XMLHttpRequest",
-        Accept: "application/json"
-    }
+  headers: {
+    'X-CSRF-TOKEN': '',
+    'X-Requested-With': 'XMLHttpRequest',
+    Accept: 'application/json'
+  }
 });
 
 const cache = new InMemoryCache();
 
 // Create the apollo client
 const apollo = new ApolloClient({
-    link: httpLink,
-    cache,
-    connectToDevTools: true
+  link: httpLink,
+  cache,
+  connectToDevTools: true
 });
 
 export default new VueApollo({
-    defaultClient: apollo
+  defaultClient: apollo
 });
